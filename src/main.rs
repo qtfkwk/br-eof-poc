1e41b120dc735b3d372d8da7ae25463211b539c7
qtfkwk <qtfkwk@gmail.com>
2022-05-31 09:30:00 -0400

    add cat, animal enum, and pets

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,25 @@ struct Pack {
     dogs: Vec<Dog>,
 }
 
+#[derive(BinRead)]
+#[br(magic = b"CAT", assert(name.len() != 0))]
+struct Cat {
+    #[br(pad_before = 0x2)]
+    name: NullString
+}
+
+#[derive(BinRead)]
+enum Animal {
+    Dog(Dog),
+    Cat(Cat),
+}
+
+#[derive(BinRead)]
+struct Pets {
+    #[br(parse_with = until_eof)]
+    list: Vec<Animal>,
+}
+
 fn main() {
     let mut reader = Cursor::new(b"DOG\x02\x00\x01\x00\x12\0\0Rudy\0");
     let dog: Dog = reader.read_ne().unwrap();
@@ -42,4 +61,30 @@ fn main() {
     assert_eq!(pack.dogs[0].name.clone().into_string(), "Rudy");
     assert_eq!(pack.dogs[1].bone_piles, &[0x45]);
     assert_eq!(pack.dogs[1].name.clone().into_string(), "Killer");
+
+    // Create a cat...
+    let mut reader = Cursor::new(b"CAT\0\0Fluffy\0");
+    let cat: Cat = reader.read_ne().unwrap();
+    assert_eq!(cat.name.into_string(), "Fluffy");
+
+    // Try to create a group of pets...
+    let mut reader = Cursor::new(b"DOG\x02\x00\x01\x00\x12\0\0Rudy\0CAT\0\0Fluffy\0DOG\x01\x00\x45\0\0Killer\0");
+    let pets: Pets = reader.read_ne().unwrap();
+    // Blows up with:
+    // thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: EnumErrors { pos:
+    // 0x2A, variant_errors: [("Dog", Io(Error { kind: UnexpectedEof, message: "failed to fill
+    // whole buffer" })), ("Cat", Io(Error { kind: UnexpectedEof, message: "failed to fill whole
+    // buffer" }))] }', src/main.rs:72:39
+    assert_eq!(pets.list.len(), 3);
+    if let Animal::Dog(dog) = &pets.list[0] {
+        assert_eq!(dog.bone_piles, &[0x1, 0x12]);
+        assert_eq!(dog.name.clone().into_string(), "Rudy");
+    }
+    if let Animal::Cat(cat) = &pets.list[1] {
+        assert_eq!(cat.name.clone().into_string(), "Fluffy");
+    }
+    if let Animal::Dog(dog) = &pets.list[2] {
+        assert_eq!(dog.bone_piles, &[0x45]);
+        assert_eq!(dog.name.clone().into_string(), "Killer");
+    }
 }
